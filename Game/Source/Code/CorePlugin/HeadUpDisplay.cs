8a297273d813a0c3ace3ec794ef8b19810dec5a7
Adam <adam@adamslair.net>
2017-12-02 18:59:25 +0100

    HUD Radar Element

--- Game/Source/Code/CorePlugin/HeadUpDisplay.cs
+++ Game/Source/Code/CorePlugin/HeadUpDisplay.cs
@@ -27,8 +27,10 @@ namespace Game
 			Ship playerShip = player.ControlTarget;
 
 			Canvas canvas = new Canvas(device);
+			ColorRgba baseColor = ColorRgba.White.WithAlpha(0.75f);
+			ColorRgba defaultColor = this.displayedTeamColor.ToColor() * baseColor;
 			canvas.State.SetMaterial(new BatchInfo(DrawTechnique.Alpha, ColorRgba.White));
-			canvas.State.ColorTint = this.displayedTeamColor.ToColor() * ColorRgba.White.WithAlpha(0.75f);
+			canvas.State.ColorTint = defaultColor;
 
 			// Health bar
 			float healthBarHeight = 150;
@@ -50,6 +52,38 @@ namespace Game
 				canvas.Height - 10 - energyBarHeight * playerShip.WeaponEnergy,
 				5,
 				energyBarHeight * playerShip.WeaponEnergy);
+
+			// Radar
+			float radarDisplayRadius = 100.0f;
+			Rect radarArea = Rect.Align(
+				Alignment.BottomRight, 
+				canvas.Width - 10, 
+				canvas.Height - 10, 
+				radarDisplayRadius * 2.0f, 
+				radarDisplayRadius * 2.0f);
+			float radarRange = 2500.0f;
+			canvas.State.ColorTint = defaultColor * ColorRgba.Black.WithAlpha(0.25f);
+			canvas.FillOval(
+				radarArea.X,
+				radarArea.Y,
+				radarArea.W,
+				radarArea.H);
+			foreach (Ship ship in player.GameObj.ParentScene.FindComponents<Ship>())
+			{
+				Vector3 relativePos = ship.GameObj.Transform.Pos - playerShip.GameObj.Transform.Pos;
+				Vector3 normalizedPos = relativePos / radarRange;
+				Vector2 posOnRadar = radarArea.Center + (normalizedPos * radarDisplayRadius).Xy;
+
+				canvas.State.ColorTint = baseColor * ship.TeamColor;
+				canvas.FillCircle(posOnRadar.X, posOnRadar.Y, 3.0f);
+			}
+			canvas.State.ColorTint = defaultColor;
+			canvas.DrawOval(
+				radarArea.X,
+				radarArea.Y,
+				radarArea.W,
+				radarArea.H);
+			canvas.FillCircle(radarArea.CenterX, radarArea.CenterY, 3.0f);
 		}
 
 		void ICmpUpdatable.OnUpdate()
